da6928b6cd8ee9f5f86bb7304d84ed94124c29eb
Gil Arroteia <47061119+Gil124@users.noreply.github.com>
2024-08-27 22:58:18 +0100

    join us page with benefits section added

--- src/pages/JoinUs.jsx
+++ src/pages/JoinUs.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+import styles from "./JoinUs.module.css";
+import heroImage from "../assets/joinUs.svg";
+import { getContent } from "../components/Navbar/Navbar";
 
 function JoinUs() {
   return (
-    <div>JoinUs</div>
-  )
+    <>
+      <div className={styles.hero_section}>
+        <div className={styles.left_container}>
+          <img src={heroImage}></img>
+        </div>
+        <div className={styles.right_container}>
+          <h1 className={styles.title}>{getContent().join.content[0].title}</h1>
+          <p className={styles.description}>{getContent().join.content[0].description}</p>
+          <h3 className={styles.catch}>{getContent().join.content[0].catch}</h3>
+        </div>
+        <div className={styles.left_corner_white}></div>
+      </div>
+      <div className={styles.relats_section}>
+        <div className={styles.right_corner_blue}></div>
+      </div>
+    </>
+  );
 }
 
-export default JoinUs
+export default JoinUs;
